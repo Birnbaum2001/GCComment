fb370038d1dd9cf8f29fb582d26ede9a288067da
Birnbaum2001 <birnbaum2001@gmx.de>
2014-07-03 22:12:08 +0200

    release 84

--- src/gccomment.user.js
+++ src/gccomment.user.js
@@ -11,7 +11,7 @@
 // @grant				GM_listValues
 // @grant				GM_registerMenuCommand
 // @grant				GM_log
-// @version			83
+// @version			84
 // @author			Birnbaum2001
 // ==/UserScript==
 
@@ -50,7 +50,7 @@
 // version information
 var $ = unsafeWindow.$;
 var jQuery = unsafeWindow.jQuery;
-var version = 83;
+var version = 84;
 var updatechangesurl = 'https://raw.githubusercontent.com/Birnbaum2001/GCComment/master/src/version.json';
 var updateURL = "https://raw.githubusercontent.com/Birnbaum2001/GCComment/master/src/gccomment.user.js";
 
